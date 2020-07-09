344fc603cd7a216c6a7ece597d78ca36689eadc6
Bhavya Trivedi <60940701+bhavyatrivedi7@users.noreply.github.com>
2020-07-09 20:05:11 +0530

    Update rpc.py

--- rpc.py
+++ rpc.py
@@ -1,31 +1,57 @@
-from random import*
+from random import randint
+val = 0
+vals = 0
 rounds=int(input("select rounds from 1 to 5"))
 for i in range(1,6):
     if(rounds+1==i):
         print("match over")
+        if vals > val:
+            print("bhavya win:", vals)
+        elif vals < val:
+            print("computer win", val)
+        else:
+            print("game is tie")
         break
-    t = ["rock", "paper", "scissors"]
-    computer = t[randint(0,2)]
+    t = ['rock', 'paper', 'scissor']
+    computer = t[randint(0, 2)]
     player = False
     while player == False:
-        player = input("rock, paper, scissors?..Choose one: ")
+        player = input("Rock,Paper,Scissor?")
+        print("computer choice is :", computer)
         if player == computer:
-            print("Tie!")
-        elif player == "rock":
-            if computer == "paper":
-                print("You lose!", computer, "covers", player)
+            print("the game is tie")
+        elif player == 'rock':
+            if computer == 'paper':
+                print("computer wins!!!!!!!!")
+                val = val + 1
+
             else:
-                print("You win!", player, "smashes", computer)
-        elif player == "paper":
-            if computer == "scissors":
-                print("You lose!", computer, "cut", player)
+                print("bhavya wins!!!!!!!!!")
+                vals = vals + 1
+
+
+        elif player == 'scissor':
+            if computer == 'rock':
+                print("computer wins!!!!!!")
+                val = val + 1
+
+
             else:
-                print("You win!", player, "covers", computer)
-        elif player == "scissors":
-            if computer == "rock":
-                print("You lose...", computer, "smashes", player)
+                print("bhavya wins!!!!!!!!")
+                vals = vals + 1
+
+        elif player == 'paper':
+            if computer == 'scissor':
+                print("computer wins!!!!!!!!")
+                val = val + 1
+
             else:
-                print("You win!", player, "cut", computer)
+                print("bhavya wins!!!!!!!!")
+                vals = vals + 1
+
         else:
-            print("That's not a valid play. Check your spelling!")
-            player = False
+            print("Something went wrong")
+    player = False
+    computer = t[randint(0, 2)]
+
+print("thanks for playing")
